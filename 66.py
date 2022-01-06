3b89acef4626dfc225de25f803133da2281e9ce9
LipatoS <sasharubaiko@gmail.com>
2022-01-06 21:39:04 +0200

    new_commit_06.01.2022

--- 66.py
+++
@@ -1,3 +0,0 @@
-lst = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
-for k, i in enumerate(lst):
-    print([i[k] for i in lst])
--- app/currency/models.py
+++ app/currency/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from currency import model_choises as MCH
 
+
+class SourceModel(models.Model):
+    name = models.CharField(max_length=24)
+
+
 class Rate(models.Model):
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=21)
@@ -19,10 +24,10 @@ class ContactUs(models.Model):
 
 
 class Rate2(models.Model):
-
     sale = models.DecimalField(max_digits=5, decimal_places=2)
     buy = models.DecimalField(max_digits=5, decimal_places=2)
-    source = models.CharField(max_length=16)
+    # source = models.CharField(max_length=16)
+    source = models.ForeignKey(SourceModel, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     type = models.CharField(max_length=3, choices=MCH.RATE2_TYPES)
 
